d63959840d089d5cd2ae96e5bc8b807f08329aeb
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-09-07 19:45:47 +0200

    initiated credential class

--- Locker.py
+++ Locker.py
@@ -39,3 +39,16 @@ class Credentials :
                 current_user = user.username
 
                 return current_user
+    
+
+    def __init__(self, user_name, site_name, account_name, password):
+            '''
+            Method to define the properties for each user object will hold.
+            '''
+
+            # ***** instance variables *****#
+
+            self.user_name = user_name
+            self.site_name = site_name
+            self.account_name = account_name
+            self.password = password
