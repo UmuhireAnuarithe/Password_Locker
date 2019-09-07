50c1daeca6ecc506bf98a48b191478a6b9cecb06
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-09-07 18:18:22 +0200

    created subtest class

---
+++ Locker-test.py
@@ -0,0 +1,10 @@
+from Locker-test.py import  Account
+
+
+
+class TestAccout(unittest.TestCase):
+    '''
+    Test class that defines test cases for the accout class behaviours.
+    Args:
+        unittest.TestCase: helps in creating test cases
+    '
