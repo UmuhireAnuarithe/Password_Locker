bf44370177dfbc81bd4bba22387f663bb3646b72
UmuhireAnuarithe <anuarithemuhire@gmail.com>
2019-09-07 18:44:47 +0200

    added setup  funnctionn  with test  for  instance initialization

--- Locker-test.py
+++ Locker-test.py
@@ -1,10 +1,31 @@
-from Locker-test.py import  Account
+from Locker import  Account
 
-
+import unittest
 
 class TestAccout(unittest.TestCase):
     '''
     Test class that defines test cases for the accout class behaviours.
     Args:
-        unittest.TestCase: helps in creating test cases
-    '
+    unittest.TestCase: helps in creating test cases
+    '''
+
+
+    def setUp(self):
+        '''
+        Function to create a user account before each test
+        '''
+        self.new_user = Account('Umuhire','Anuarithe','111')
+
+
+    def test__init__(self):
+        '''
+        Test to if check the initialization/creation of accounnt   instances is properly done
+        '''
+        self.assertEqual(self.new_user.fullname, 'Umuhire')
+        self.assertEqual(self.new_user.username, 'Anuarithe')
+        self.assertEqual(self.new_user.password, '111')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
